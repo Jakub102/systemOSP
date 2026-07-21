1df283251e90e3a6a54144588364adcc990e31bf
Bahraz <jarekg0626@gmail.com>
2026-07-22 00:44:49 +0200

    Refactor: rebuild Verify and Cancellation Controller for invitation

--- backend/lang/pl/invitation.php
+++ backend/lang/pl/invitation.php
@@ -4,4 +4,6 @@ return [
     'email_unique' => 'Konto z tym adresem e-mail jest już zarejestrowane w systemie.',
     'created' => 'Zaproszenie zostało pomyślnie wysłane.',
     'updated' => 'Zaproszenie istniało w bazie. Token, czas wygaśnięcia oraz dane zostały zaktualizowane, a mail wysłany ponownie.',
+    'invalid_or_expired' => 'Token jest nieprawidłowy, został już użyty lub wygasł.',
+    'cancelled' => 'Zaproszenie zostało pomyślnie anulowane.',
 ];
