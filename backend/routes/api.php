6074f1f9b86acfbe6946513a4427fe960fac6de2
Bahraz <jarekg0626@gmail.com>
2026-07-21 20:37:18 +0200

    Refactor: logout controller rebuild

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,16 +1,31 @@
 <?php
-
+//TODO: Do usunięcia 
 use App\Http\Controllers\AuthController;
+
 use App\Http\Controllers\InvitationController;
 use Illuminate\Support\Facades\Route;
 
+//TODO: Te poniżej są przebudowane
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
+
 // W routes/api.php
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1'); // max 5 prób na minutę
+// Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1'); // max 5 prób na minutę
+
+
+Route::prefix('v1')->group(function(){
+
+    Route::post('/login', LoginController::class)->middleware('throttle:5,1');
+
+    Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
+            Route::post('/logout', LogoutController::class);
+        });
+    });
 
 Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
     Route::post('/auth/refresh', [AuthController::class, 'refresh']);
     Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    // Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
